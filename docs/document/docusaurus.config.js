e11801cfd59bb9af755640c0fe96fa4f4df26d83
Nguyen Hoang Anh <nmanhnit@gmail.com>
2023-03-28 13:18:05 +0700

    update organization name and peoject name

--- docs/document/docusaurus.config.js
+++ docs/document/docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kintone-labs', // Usually your GitHub org/user name.
-  projectName: 'kintone-ui-component', // Usually your repo name.
+  organizationName: 'Cybozu-SD', // Usually your GitHub org/user name.
+  projectName: 'kuc-docusaurus-v2', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
